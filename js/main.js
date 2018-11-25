55c0fb5103b73aca27c8522182048c52fdadb1f1
taki <takiguru.wow@gmail.com>
2018-11-25 23:44:38 +0100

    Import js classes and initialize them

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let videoFile = new VideoData(
+	'http://www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/10sec/bunny_317328bps/',
+	'BigBuckBunny',
+	10,
+	1,
+	60,
+	'video/mp4; codecs="avc1.4d401f"'
+);
+let Player = new VideoPlayer(videoFile);
+let Controls = new CustomControls(Player.video);
