5ae6e0c6bf1d42a5f9aad59dbe1b775f78dc78ef
taki <takiguru.wow@gmail.com>
2018-11-25 23:42:23 +0100

    Add customControls class

---
+++ js/customControls.js
@@ -0,0 +1,45 @@
+class CustomControls {
+	constructor(video) {
+		this.enabled = false;
+		this.ref = video;
+		if (!this.ref) throw 'Missing video reference in CustomControls class';
+		this.enableSwitch = document.getElementById('customControlsEnabled');
+		if (!this.enableSwitch) throw 'Missing enableSwitch reference in CustomControls class';
+		this.volumeSlider = document.getElementById('customControlsVolumeSlider');
+		if (!this.volumeSlider) throw 'Missing volumeSlider reference in CustomControls class';
+		this.divElement = document.getElementById('customControlsContainer');
+		if (!this.divElement) throw 'Missing divElement reference in CustomControls class';
+		this.bindEvents();
+	}
+	bindEvents() {
+		this.volumeSlider.addEventListener('input', this.setVolume.bind(this));
+		this.enableSwitch.addEventListener('change', this.switchState.bind(this));
+	}
+	switchVisibility() {
+		if (this.enabled) {
+			this.divElement.style.display = 'none';
+		} else {
+			this.divElement.style.display = 'block';
+		}
+	}
+	switchState() {
+		this.switchVisibility();
+		this.ref.controls = !this.ref.controls;
+		this.enabled = !this.enabled;
+	}
+	setVolume(e) {
+		this.ref.volume = e.target.value;
+	}
+	changePlayState() {
+		if (this.ref.paused) {
+			this.ref.play();
+		} else {
+			this.ref.pause();
+		}
+	}
+	seekVideo(value) {
+		if (this.ref.currentTime + value < this.ref.duration) {
+			this.ref.currentTime = this.ref.currentTime + value;
+		}
+	}
+}
